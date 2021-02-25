579c894826e87e635c4bc4e6263cc0509975c527
Zack Johnson <zackmj99@gmail.com>
2021-02-24 22:20:41 -0600

    a bit of an outline

--- src/astar/Master.java
+++ src/astar/Master.java
@@ -3,6 +3,8 @@ package astar;
 public class Master {
 	
 	public static void main(String[] args) {
-		
+		//convert data file to graph
+		//run A* on graph
+		   // which will involve calls to Heuristic
 	}
 }
